026dbfdd7eeebe57ee7a7fd4d78811c28c3d3c91
R-V-Abhishek <rvabhi2504@gmail.com>
2025-03-03 19:57:55 +0530

    for hosting

--- backend/loginserver.js
+++ backend/loginserver.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://saffronspicerestaurant.netlify.app', 'http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://saffron-spice-restaurant-n6czdwbcv-r-v-abhisheks-projects.vercel.app'],
   credentials: true
 }));
 app.use(express.json()); // Parse JSON payloads
